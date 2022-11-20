7b7e2f07c5cf297579d53060e4d896d9b4d878c4
Junkynioy <33281422+JunkynioyPH@users.noreply.github.com>
2022-11-20 14:47:28 +0000

    replit broke

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
 import os
 
-# The hello world that is written by REPLIT.com
-# New modifications from REPLIT.com for testing
 print("Hello world from ...")
-os.system("python --version")
-
-# Hello world that i copy pasted in VSCODE synced using GIT
-print("Hello world from VSCODE!")
 os.system("python --version")
