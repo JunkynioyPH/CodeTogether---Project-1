efd6c8dcd7125b58b089de40ff900885de923ef9
Junkynioy <virgil_bravo@yahoo.com>
2022-11-20 22:34:09 +0800

    test commit from VSCODE

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ import os
 # New modifications from REPLIT.com for testing
 print("Hello world from ...")
 os.system("python --version")
+
+# Hello world that i copy pasted in VSCODE synced using GIT
+print("Hello world from VSCODE!")
+os.system("python --version")
