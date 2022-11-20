c321542ce48060893ff199653630bb0aa6918207
Junkynioy <33281422+JunkynioyPH@users.noreply.github.com>
2022-11-20 15:20:32 +0000

    added a comment to certify my achievemet

--- main.py
+++ main.py
@@ -9,3 +9,8 @@ os.system("python --version")
 # Test
 print("Hello world from VSCODE!")
 os.system("python --version")
+
+## This comment is to certify that i have successfully
+## configured the .gitignore file to actually ignore replit's files from being
+## commited to main in github and so that in VSCODE, stuff will be cleaner in the
+## working directory! YAY! it only took 2.5 hours to figure out!
